5ca157bb866078d630de27f3bb0eed5759e68401
Invisible16 <julia.nguyen0616@gmail.com>
2020-07-20 07:36:40 +0700

    set up project

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {Provider} from 'react-redux';
+import {createStore, applyMiddleware} from 'redux';
+import rootReducer from './reducers'
+import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga'
+import rootSaga from './sagas';
+const sagaMiddleware=createSagaMiddleware();
+const store = createStore(rootReducer,applyMiddleware(sagaMiddleware,logger))
+sagaMiddleware.run(rootSaga)
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
