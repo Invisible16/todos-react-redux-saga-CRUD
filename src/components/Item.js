20329059ceb9bdf249af6febe9bf52ef772bb930
Invisible16 <julia.nguyen0616@gmail.com>
2020-07-27 11:11:33 +0700

    modify update

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,15 +1,15 @@
 import React, { useState } from 'react'
 import './Item.css'
 export default function Item(props) {
-    let { item } = props
+    let { item, isChecked } = props
     return (
         <tr>
-            <td><input type="checkbox" 
-             onChange={(e) => {
-                if (e.target.checked) {props.updateChange(item)}
-                else props.updateChange({name:'',percent:''})
-                
-            }} 
+            <td><input type="checkbox" checked={isChecked }
+                onChange={(e) => {
+                    if (e.target.checked) { props.updateChange(item) }
+                    else props.updateChange({ name: '', percent: '' })
+
+                }}
             /> </td>
             <td>{item.id}</td>
             <td> {item.name}
--- src/components/Items.js
+++ src/components/Items.js
@@ -3,11 +3,15 @@ import Item from './Item'
 import './Item.css'
 export default class Items extends Component {
     state = {
-        name: "",
-        percent: ''
+        updateData: {
+            name: "",
+            percent: ''
+        },
+        indexChoose: -1
     }
     render() {
         let { items } = this.props
+        let { updateData } = this.state
         // console.log("item component::::", items);
         return (
             <div>
@@ -34,24 +38,27 @@ export default class Items extends Component {
                             </tr>
                             {(items) && items.map((item, index) =>
                                 <Item
-                                    updateChange={(newData) => { this.setState(newData) }}
-                                    item={item} key={index} />)}
+                                    updateChange={(newData) => { this.setState({ updateData: newData, indexChoose: index }) }}
+                                    item={item} key={index}
+                                    isChecked={this.state.indexChoose === index}
+                                />)}
                         </tbody>
                     </table>
                     <form action="">
+                        <h3>Id item : {updateData.id}</h3>
                         <label htmlFor="name">Name:</label><br />
-                        <input type="text" id="name" name="name" defaultValue={this.state.name}
+                        <input type="text" id="name" name="name" defaultValue={updateData.name}
                             onChange={(e) => {
-                                this.setState({ name: e.target.value })
+                                this.setState({ updateData: { ...updateData, name: e.target.value } })
                             }}
                         /><br />
                         <label htmlFor="percent">Percent:</label><br />
-                        <input type="text" id="percent" name="percent" defaultValue={this.state.percent}
+                        <input type="text" id="percent" name="percent" defaultValue={updateData.percent}
                             onChange={(e) => {
-                                this.setState({ percent: e.target.value })
+                                this.setState({ updateData: { ...updateData, percent: e.target.value } })
                             }} /><br /><br />
                         <button onClick={() => {
-                            this.props.updateDispatch(this.state)
+                            this.props.updateDispatch(updateData)
                             console.log('update')
                         }}>Update</button>
                     </form>
